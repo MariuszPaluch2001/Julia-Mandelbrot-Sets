9ba8f9850755566a370baae31338ce10557e141a
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-07-14 18:06:48 +0200

    Add calculate_z_serial function.

--- src/julia_set.py
+++ src/julia_set.py
@@ -20,3 +20,16 @@ def yield_complex_cord(xs, ys):
     for y_cord in ys:
         for x_cord in xs:
             yield complex(x_cord, y_cord)
+
+
+def calculate_z_serial(max_iteration, xs, ys, c):
+    output = []
+    for z in yield_complex_cord(xs, ys):
+        n = 0
+        while abs(z) < 2 and n < max_iteration:
+            z = z**2 + c
+            n += 1
+
+        output.append(n)
+
+    return output
