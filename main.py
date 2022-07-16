57fa1fc900b3f34ddc61a2eb25ceb60119af0cf2
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-07-16 11:35:58 +0200

    Interval function is using now generator.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.utilities import generate_plot
 if __name__ == "__main__":
     x1, x2, y1, y2 = -1.8, 1.8, -1.8, 1.8
     c = complex(-0.75, 0.1)
-    # xs, ys, F = generate_julia_set(x1,x2,y1,y2, c, 300, 1000)
-    # generate_plot(xs,ys,F)
+    xs, ys, F = generate_julia_set(x1,x2,y1,y2, c, 300, 1000)
+    generate_plot(xs,ys,F)
     xs, ys, F = generate_mandelbrot_set(x1,x2,y1,y2, 300, 1000)
     generate_plot(xs,ys,F)
--- src/utilities.py
+++ src/utilities.py
@@ -15,21 +15,16 @@ def timefn(fn):
         return result
     return measure_time
 
+def interval_generator(a, b, width):
+    epsilon = abs(a - b) / width
+    cord, up_limit = min(a, b), max(a, b)
+    while cord < up_limit:
+        yield cord
+        cord += epsilon
+
 def list_interval(x1, x2, y1, y2, desired_width):
-    x_epsilon = abs(x2 - x1) / desired_width
-    y_epsilon = abs(y2 - y1) / desired_width
-
-    xs = []
-    x_cord, x_up_limit = min(x1, x2), max(x1, x2)
-    while x_cord < x_up_limit:
-        xs.append(x_cord)
-        x_cord += x_epsilon
-
-    ys = []
-    y_cord, y_up_limit = min(y1, y2), max(y1, y2)
-    while y_cord < y_up_limit:
-        ys.append(y_cord)
-        y_cord += y_epsilon
+    xs = list(interval_generator(x1, x2, desired_width))
+    ys = list(interval_generator(y1, y2, desired_width))
 
     return xs, ys
 
